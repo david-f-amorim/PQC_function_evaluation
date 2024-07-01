73d0690b986883a451a8b6dd4185bfd572b0715a
2538354a <122365024+david-f-amorim@users.noreply.github.com>
2024-07-01 12:01:08 +0100

    first attempts

---
+++ plots.py
@@ -0,0 +1,86 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib import rcParams
+import os 
+
+# general settings
+rcParams['mathtext.fontset'] = 'stix'
+rcParams['font.family'] = 'STIXGeneral'
+width=0.75
+color='black'
+fontsize=16
+ticksize=22
+figsize=(10,10)
+
+# import data
+loss_file = os.path.join("outputs","loss_2_2_2_2000.npy")
+mismatch_file = os.path.join("outputs","mismatch_2_2_2_2000.npy")
+
+# configure arrays
+loss_arr = np.load(loss_file)
+mismatch_arr = np.load(mismatch_file)
+fidelity_arr = 1- mismatch_arr
+epochs = np.arange(len(loss_arr))+1
+
+# set up plots 
+fig, axs = plt.subplots(1,3,figsize = [15,10])
+fig.subplots_adjust(left=0.05, bottom=0.15, right=0.95, hspace=.65)
+axs = axs.ravel()
+
+arrs= [loss_arr, mismatch_arr, fidelity_arr]
+labels = ["Loss", "Mismatch", "Fidelity"]
+
+for i in np.arange(len(axs)):
+
+    axs[i].scatter(epochs, arrs[i], color="black", s=10)
+    axs[i].set_ylabel(labels[i], fontsize=fontsize)
+    axs[i].set_xlabel("Epochs", fontsize=fontsize)
+
+plt.savefig(os.path.join("plots","plots_2_2_2_2000.png"), dpi=500)
+#plt.show()
+
+# get indices of three regions 
+seed = 1680458526
+rng = np.random.default_rng(seed=seed)
+
+n =2 
+epochs = 2000 
+
+x_min = 0
+x_max = 2**n 
+x_arr = rng.integers(x_min, x_max, size=epochs)
+
+
+low_mismatch_cluster = np.where(mismatch_arr < 0.45)
+mid_mismatch_cluster = np.where((mismatch_arr > 0.45) * (mismatch_arr < 0.8))
+high_mismatch_cluster = np.where(mismatch_arr > 0.8)
+
+low_x = [] 
+mid_x = []
+high_x = [] 
+
+for i in low_mismatch_cluster:
+
+    low_x.append(x_arr[i])
+
+for i in mid_mismatch_cluster:
+
+    mid_x.append(x_arr[i])
+
+for i in high_mismatch_cluster:
+
+    high_x.append(x_arr[i]) 
+
+unique, counts = np.unique(low_x, return_counts=True)    
+low_dict = dict(zip(unique, counts))
+
+unique, counts = np.unique(mid_x, return_counts=True)    
+mid_dict = dict(zip(unique, counts))
+
+unique, counts = np.unique(high_x, return_counts=True)    
+high_dict = dict(zip(unique, counts))
+         
+print("low",low_dict)
+print("mid",mid_dict)
+print("high",high_dict)
+
--- tools.py
+++ tools.py
@@ -1,7 +1,15 @@
 import numpy as np 
-from qiskit import QuantumCircuit, QuantumRegister 
+from qiskit import QuantumCircuit, QuantumRegister, Aer, execute 
 from qiskit.circuit import ParameterVector
 from itertools import combinations
+from qiskit_machine_learning.neural_networks import SamplerQNN
+from qiskit_machine_learning.connectors import TorchConnector
+from qiskit.utils import algorithm_globals
+from qiskit.primitives import Sampler
+from torch.optim import Adam
+from torch.nn import MSELoss
+from torch import Tensor, no_grad 
+import sys, time, os 
 
 """
 Construct an input layer consisting of controlled Ry rotations
@@ -190,7 +198,7 @@ Both the input state and the circuit weights can be set by accessing circuit par
 after initialisation.  
 """
 
-def generate_network(n,m,L, encode=True):
+def generate_network(n,m,L, encode=False):
 
     # initialise empty input and target registers 
     input_register = QuantumRegister(n, "input")
@@ -219,11 +227,141 @@ def generate_network(n,m,L, encode=True):
 
     return circuit 
 
+"""
+Initialise circuit as QNN for training purposes.
+"""
+
+def train_QNN(n,m,L, seed, shots, lr, b1, b2, epochs, func):
+
+    # set seed for PRNG 
+    algorithm_globals.random_seed= seed
+    rng = np.random.default_rng(seed=seed)
+
+    # generate circuit and set up as QNN 
+    qc = generate_network(n,m,L, encode=True)
+    qnn = SamplerQNN(
+            circuit=qc.decompose(),             # decompose to avoid data copying (?)
+            sampler=Sampler(options={"shots": shots, "seed": algorithm_globals.random_seed}),
+            input_params=qc.parameters[:n],   # encoding params treated as input params
+            weight_params=qc.parameters[n:],   # encoding params not selected as weights
+            input_gradients=True # ?? 
+        )
+
+    # choose random initial weights and initalise TorchConnector 
+    initial_weights = algorithm_globals.random.random(len(qc.parameters[n:]))
+    model = TorchConnector(qnn, initial_weights)
+
+    # choose optimiser and loss function 
+    optimizer = Adam(model.parameters(), lr=lr, betas=(b1, b2), weight_decay=0.005) # Adam optimizer 
+    criterion = MSELoss(reduction="sum") # MSE loss 
+
+    # set up arrays to store training outputs 
+    mismatch_vals = np.empty(epochs)
+    loss_vals = np.empty(epochs)
+
+    # generate x and f(x) values (IMPROVE LATER!!)
+    x_min = 0
+    x_max = 2**n 
+    x_arr = rng.integers(x_min, x_max, size=epochs)
+    fx_arr = [func(i) for i in x_arr]
+
+    # start training 
+    print("\n\nTraining started \n")
+    start = time.time() 
+
+    for i in np.arange(epochs):
+
+        # get input data
+        input = Tensor(binary_to_encode_param(np.binary_repr(x_arr[i],n))) 
+
+        # get target data (RETHINK THIS!!!)
+        target_arr = np.zeros(2**(n+m))
+        index = int(np.binary_repr(x_arr[i],n)+np.binary_repr(fx_arr[i],m),2)
+        target_arr[index]=1 
+        target=Tensor(target_arr)
+
+        # train model  
+        optimizer.zero_grad()
+        loss = criterion(model(input), target)
+        loss.backward()
+        optimizer.step()
+
+        # save loss for plotting 
+        loss_vals[i]=loss.item()
+
+        # set up circuit with calculated weights
+        circ = generate_network(n,m,L, encode=True)
+
+        with no_grad():
+            generated_weights = model.weight.detach().numpy()
+
+        input_params = binary_to_encode_param(np.binary_repr(x_arr[i],n))
+        params = np.concatenate((input_params, generated_weights))    
+        
+        circ = circ.assign_parameters(params)    
+
+        # get statevector 
+        backend = Aer.get_backend('statevector_simulator')
+        job = execute(circ, backend)
+        result = job.result()
+        state_vector = np.asarray(result.get_statevector()) 
+
+        # calculate fidelity and mismatch 
+        fidelity = np.abs(np.dot(np.sqrt(target_arr),np.conjugate(state_vector)))**2
+        mismatch = 1. - np.sqrt(fidelity)
+
+        # save mismatch for plotting 
+        mismatch_vals[i]=mismatch
+
+        # print status
+        a = int(20*(i+1)/epochs)
+
+        if i==0:
+            time_str="--:--:--.--"
+        elif i==epochs-1:
+            time_str="00:00:00.00"    
+        else:
+            remaining = ((time.time() - start) / i) * (epochs - i)
+            mins, sec = divmod(remaining, 60)
+            hours, mins = divmod(mins, 60)
+            time_str = f"{int(hours):02}:{int(mins):02}:{sec:05.2f}"
+
+        prefix="\t" 
+        print(f"{prefix}[{u'█'*a}{('.'*(20-a))}] {100.*((i+1)/epochs):.2f}% ; Loss {loss_vals[i]:.2f} ; Mismatch {mismatch:.2e} ; ETA {time_str}", end='\r', file=sys.stdout, flush=True)
+        
+        
+    print(" ", flush=True, file=sys.stdout) 
+
+    elapsed = time.time()-start
+    mins, sec = divmod(elapsed, 60)
+    hours, mins = divmod(mins, 60)
+    time_str = f"{int(hours):02}:{int(mins):02}:{sec:05.2f}" 
+
+    print(f"\nTraining completed in {time_str}\n\n")
+
+    # save outputs (FIND BETTER NAMING CONVENTIONS!)
+    with no_grad():
+            generated_weights = model.weight.detach().numpy()
+
+    np.save(os.path.join("outputs", "weights_"+str(int(n))+"_"+str(int(m))+"_"+str(int(L))+"_"+str(int(epochs))),generated_weights)
+    np.save(os.path.join("outputs", "mismatch_"+str(int(n))+"_"+str(int(m))+"_"+str(int(L))+"_"+str(int(epochs))),mismatch_vals)
+    np.save(os.path.join("outputs", "loss_"+str(int(n))+"_"+str(int(m))+"_"+str(int(L))+"_"+str(int(epochs))),loss_vals)
+    
+    return 0 
+
+"""
+Analytical test function
+"""
+
+def f(x):
+
+    return x
+
 ####
 
-### use samplerQNN ; maybe split up encoding circuit for easier inputs v weights distiction ...
+train_QNN(n=2,m=2,L=10, seed=1680458526, shots=500, lr=0.01, b1=0.7, b2=0.99, epochs=2000, func=f)
 
-circ = generate_network(3,4,5)
 
-print(circ.ParameterVector("enc"))
+
+
 
