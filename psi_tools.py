9ebe7389e4b13b1c57e5431610388e5ea28b0385
2538354a <122365024+david-f-amorim@users.noreply.github.com>
2024-07-09 16:56:58 +0100

    added binary encoding

--- psi_tools.py
+++ psi_tools.py
@@ -4,127 +4,35 @@ import numpy as np
 FUNCTIONS ASSOCIATED WITH CLASSICAL BINARY OPERATIONS 
 """
 
-def get_nint(digits):
-    if np.array(digits).ndim==0:
-        digits=np.array([digits])
-    digits = np.where(np.abs(digits)>1.,np.modf(digits)[1],digits)
-    digits = digits[digits!=0.]
-    if len(digits)==0:
-        return 0
-    nint = int(np.ceil(np.log2(np.max(np.abs(digits))))) + 1
-    #if np.max(np.abs(digits))<np.sum(np.power(2., np.arange(nint-1))):
-    #    nint-=1
-    return nint
-
-def get_npres(digits):
-    if np.array(digits).ndim==0:
-        digits=np.array([digits])
-    digdecs = np.modf(digits)[0]
-    digdecs = digdecs[digdecs!=0]
-    if len(digdecs)==0:
-        return 0
-    mindec = np.min(np.abs(digdecs))
-    switch = True
-    p = 0
-    while switch:
-        if mindec%(2.**-p)==0.:
-            switch=False
-        p+=1
-    return p-1#-int(np.floor(np.log2(np.min(np.abs(digdecs)))))
-
-
-def my_binary_repr(digit, n, nint=None, phase=False, nround=True, overflow_error=True):
-    """
-    Convert a floating point digit to binary string
-    digit - input number (float)
-    n - total number of bits (int)
-    nint - number of integer bits. Default to lowest required (int)
-    """
-
-    if nint is None:# or nint==n:
-        if phase:
-            nint = n - 1
-        else:
-            nint = n
-
-    if phase:
-        p = n - nint - 1
-        dmax = 2.**(nint) - 2.**(-p)
-        dmin = -2.**(nint)
-    else:
-        p = n - nint
-        dmax = 2.**(nint) - 2.**(-p)
-        dmin = 0.
-
-    if overflow_error:
-        if digit>dmax or digit<dmin:
-            raise ValueError('Digit '+str(digit)+' does not lie in the range:',dmin,'-',dmax,n,nint,p)
-
-    if nround:
-        n += 1
-        p += 1
-
-    value = digit
-    bin_out = ''
-    if phase:
-        if value<0.:
-            value+=2.**nint
-            bin_out+='1'
-        else:
-            bin_out+='0'
-    
-    for i,bit in enumerate(np.arange(-p,nint)[::-1]):
-        bin_out+=str(int(np.floor(value/2.**bit)))
-        if value>=2.**bit:
-            value-=2.**bit
-
-    if nround:
-        carry = True
-        bin_out = np.array(list(bin_out))
-        for i in np.arange(n)[::-1]:
-            if not carry:
-                break
-            if bin_out[i]=='1':
-                bin_out[i]='0'
-            elif bin_out[i]=='0':
-                bin_out[i]='1'
-                carry = False
-        bin_out = ("").join(list(bin_out[:-1]))
-
-    return bin_out
-
-
-def get_n(digits):
-
-
-    return nint +npres +1 
-
-def dec_to_bin(digits,n,encoding,nint=None, round=True, overflow_error=True):
-
+def dec_to_bin(digits,n,encoding,nint=None, overflow_error=True):
     """
     Encode a float `digits` in base-10 to a binary string `bits`. 
-
-    Little endian convention is used.
+     
+    Binary encoding must be specified as  `'unsigned mag'`, `'signed mag'`, 
+    or `'twos comp'` for unsigned magnitude, signed magnitude, and two's
+    complement representations, respectively. The fractional part is rounded to 
+    the available precision: unless otherwise specified via `nint`, all bits (apart 
+    from the sign bit) are assumed to be integer bits. Little endian convention is used.
     
-    ....
     """
 
-    bits=''
-
+    # set number of integer bits
     if nint==None:
         nint=n 
 
+    # at least one bit is reserved to store the sign 
     if nint==n and (encoding=='signed mag' or encoding=='twos comp'):
         nint-= 1 
 
-    if encoding=='signed mag' or encoding=='twos comp':
-        bits += ('1' if digits<0 else '0')  
+    # determine number of precision bits 
+    if encoding=='signed mag' or encoding=='twos comp': 
         p = n - nint - 1
     elif encoding=='unsigned mag':
         p = n - nint 
     else: 
         raise ValueError("Unrecognised type of binary encoding. Should be 'unsigned mag', 'signed mag', or 'twos comp'.")
 
+    # raise overflow error if float is out of range for given encoding
     if overflow_error:
 
         if encoding=='unsigned mag':
@@ -140,7 +48,39 @@ def dec_to_bin(digits,n,encoding,nint=None, round=True, overflow_error=True):
         if digits>max_val or digits<min_val:
             raise ValueError(f"Float {digits} out of available range [{min_val},{max_val}].")   
 
-    if      
+    
+    # take absolute value and separate integer and fractional part:
+    digits_int = np.modf(np.abs(digits))[1]
+    digits_frac = np.modf(np.abs(digits))[0] 
+
+    # add fractional parts
+    bits_frac=''
+    for i in range(p):
+        bits_frac +=str(int(np.modf(digits_frac * 2)[1])) 
+        digits_frac =np.modf(digits_frac * 2)[0]
+
+    # add integer parts
+    bits_int=''
+    for i in range(nint):
+        bits_int +=str(int(digits_int % 2))
+        digits_int = digits_int // 2 
+
+    bits_int= bits_int[::-1]    
+
+    if encoding=="unsigned mag":
+        bits = bits_int + bits_frac 
+    if encoding=="signed mag":
+        if digits >= 0:
+            bits = '0' +  bits_int + bits_frac
+        else:
+            bits = '1' +  bits_int + bits_frac  
+    if encoding=="twos comp":
+        if digits >=0:
+            bits = '0' +  bits_int + bits_frac
+        elif digits== min_val:
+            bits = '1' +  bits_int + bits_frac   
+        else:
+            bits = twos_complement('0' +  bits_int + bits_frac)                         
 
     return bits
 
@@ -149,7 +89,7 @@ def bin_to_dec(bits,encoding,nint=None):
     Decode a binary string `bits` to a float `digits` in base-10. 
     
     Binary encoding must be specified as 
-    `'unsigned mag'`, `'signed mag'`, or `'twos comp'` for unsigned magnitude, signed magnitude, and twos
+    `'unsigned mag'`, `'signed mag'`, or `'twos comp'` for unsigned magnitude, signed magnitude, and two's
     complement representations, respectively. Unless the number of integer bits is specified using `nint`,
     all bits (apart from the sign bit) are assumed to be integer bits. Little endian convention is used.
     """
@@ -180,16 +120,13 @@ def bin_to_dec(bits,encoding,nint=None):
 
     return digits 
 
-def twos_complement(binary, nint=None):
+def twos_complement(binary):
     """
     For a bit string `binary` calculate the two's complement binary string `compl`. 
     
-    Little endian convention is used. 
-
-    An all-zero bit string is its own complement.
-    """
-   
-    binary_to_array = list(int(binary)) 
+    Little endian convention is used. An all-zero bit string is its own complement.
+    """   
+    binary_to_array = np.array(list(binary)).astype(int)
    
     if np.sum(binary_to_array)==0:
         return binary 
@@ -200,11 +137,15 @@ def twos_complement(binary, nint=None):
     
     return compl 
 
-### 
+#####
+# 
+#  NEXT UP: 
+#   - input_bits_to_qubits ; integer_compare ; QFTBinaryAdd ; TwosCompliment ; QFTMultiply ; QFTAddition_
+#       which are required for label_gate ; first_gate ; QFTPosMultiplicand ; QFTAddition 
+#       which are required for piecewise_function_posmulti 
+ 
 
-binary = '10000'
-nint=None 
+b =dec_to_bin(-1.499999,4,nint=2, encoding="twos comp")
 
-new =dec_to_bin(1002,5,'twos comp', nint=2)
+print(b)
 
-print(new)
