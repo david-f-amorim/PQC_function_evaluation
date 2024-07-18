a1c43a6c159aa837b1cfb2495cda19a88ca50a42
2538354a <122365024+david-f-amorim@users.noreply.github.com>
2024-07-18 15:05:30 +0100

    WIM: changed filter to 0.5 sigma

--- tools.py
+++ tools.py
@@ -689,8 +689,8 @@ def train_QNN(n,m,L, seed, shots, lr, b1, b2, epochs, func,func_str,loss_str,met
                     ind = int(dec_to_bin(j,m,'unsigned mag',nint=m)+dec_to_bin(x_arr_temp[q],n,'unsigned mag',nint=nint),2) 
                     WIM_weights_arr[ind]= temp_mismatch[q]
             
-            # filter for outliers: focus on states one sigma or more above the mean
-            WIM_weights_arr *= (WIM_weights_arr >= np.mean(WIM_weights_arr)+np.std(WIM_weights_arr) ).astype(int) 
+            # filter for outliers: focus on states 0.5 sigma or more above the mean
+            WIM_weights_arr *= (WIM_weights_arr >= np.mean(WIM_weights_arr)+0.5 *np.std(WIM_weights_arr) ).astype(int) 
 
             # smoothen WIM weights 
             WIM_weights_arr=np.exp(tau_1 * WIM_weights_arr)    
