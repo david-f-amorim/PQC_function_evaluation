33790e5e561d334fbd18f1567c493a89dca6d822
2538354a <122365024+david-f-amorim@users.noreply.github.com>
2024-08-09 18:26:44 +0100

    moving towards PyPI publication

---
+++ pqcprep/resource_tools.py
@@ -0,0 +1,20 @@
+"""
+DOCUMENT THIS ...
+"""
+import numpy as np 
+import os 
+
+def load_resources(name):
+    """
+    ....
+    """
+
+    name_lists =["mismatch_QGAN_12","mismatch_QGAN_20", "amp_state_GR", "amp_state_QGAN", "full_state_GR","full_state_QGAN", "psi_LPF_processed"] 
+    #paths=[os.path.join("")] 
+
+
+ 
+
+    return 0
+
+#c:\Users\david\Documents\Code\PQC_function_evaluation\pqcprep\resources\amp_state_GR.npy c:\Users\david\Documents\Code\PQC_function_evaluation\pqcprep\resources\amp_state_QGAN.npy c:\Users\david\Documents\Code\PQC_function_evaluation\pqcprep\resources\full_state_GR.npy c:\Users\david\Documents\Code\PQC_function_evaluation\pqcprep\resources\full_state_QGAN.npy c:\Users\david\Documents\Code\PQC_function_evaluation\pqcprep\resources\mismatch_QGAN_12.npy c:\Users\david\Documents\Code\PQC_function_evaluation\pqcprep\resources\mismatch_QGAN_20.npy c:\Users\david\Documents\Code\PQC_function_evaluation\pqcprep\resources\psi_LPF_processed.npy
